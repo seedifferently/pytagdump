2e2295643d2f6125426a808bd255dfd1f81afe0e
Seth Davis <seth@curiasolutions.com>
2012-01-18 23:42:23 -0800

    Updating to v0.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except IOError:
 
 setup(
     name='pytagdump',
-    version='0.2',
+    version='0.3',
     author='Seth Davis',
     author_email='seth@curiasolutions.com',
     description="A simple utility to print the ID3/tag info of various " + \
